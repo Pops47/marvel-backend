aa8ec37c9c9dbb71ce1a7f49c2d4d55795c4f47c
Pauline Soubrié <pauline_soubrie@hotmail.fr>
2022-05-11 22:43:17 +0200

    deploy back on heroku-first test

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const formidable = require("express-formidable");
 const cors = require("cors");
 const app = express();
-
+const axios = require("axios");
 app.use(formidable());
 app.use(cors());
 
+// Welcome route
 app.get("", (req, res) => {
   res.status(200).json({
     message: "Welcome to Marvel-backend on Heroku server",
   });
 });
-
-const comicsRoute = require("./routes/comics"); //paramètres skip et title en query
+//App routes
+const comicsRoute = require("./routes/comics"); // Possible queries title and skip
 app.use(comicsRoute);
 
+//404
 app.all("*", (req, res) => {
   res.status(404).send("Page introuvable");
 });
--- routes/comics.js
+++ routes/comics.js
@@ -1,17 +1,26 @@
-require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
 router.get("/comics", async (req, res) => {
-  const limit = 100;
   try {
-    console.log(req.query);
+    const limit = 100;
+    //   Get queries and insert them in request if they exist
+    const { title, skip } = req.query;
+    let queries = "";
+    if (title) {
+      queries += `&title=${title}`;
+    }
+    if (skip) {
+      queries += `&skip=${skip}`;
+    }
+    //Send request with APIKey and queries
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}`
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}${queries}`
     );
-    // &title=${req.query.title}&title=${req.query.title} boucles pour passer les query?
-    console.log(response.data);
+    //Return answer from Marvel API
+    res.status(200).json(response.data);
+    //Handle errors
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
