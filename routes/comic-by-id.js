658976d0db5ae228f8c6f30dfd998e59746f44c2
Pauline Soubrié <pauline_soubrie@hotmail.fr>
2022-05-15 03:03:48 +0200

    add route comic-by-id

---
+++ routes/comic-by-id.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+router.get("/comic/:comicId", async (req, res) => {
+  try {
+    //   Get id in params
+    const { comicId } = req.params;
+    //Send request with APIKey and id
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/character/${comicId}?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    //Return answer from Marvel API
+    res.status(200).json(response.data);
+    //Handle errors
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
